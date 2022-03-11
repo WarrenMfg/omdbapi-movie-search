6a7fa03d430a948e44b084f0dc19ce0664c6392d
WarrenMfg <warrenmfg@icloud.com>
2022-03-11 15:15:05 -0500

    displays movie titles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
+import { fetchMovies } from '../../api/api';
+import useDispatch from '../../hooks/useDispatch';
+import useSelector from '../../hooks/useSelector';
 import './App.module.css';
 
 interface AppProps {
@@ -5,7 +9,24 @@ interface AppProps {
 }
 
 function App({ query }: AppProps) {
-  return <h2>Movie List: {query}</h2>;
+  const dispatch = useDispatch();
+  const movies = useSelector(state => state.movies[query]?.Search);
+
+  useEffect(() => {
+    dispatch(fetchMovies(query));
+  }, [query, dispatch]);
+
+  if (!movies) return <p>'Loading...'</p>;
+  return (
+    <>
+      <h2>Movie List: {query}</h2>
+      <section>
+        {movies.map(movie => (
+          <p key={movie.Title}>{movie.Title}</p>
+        ))}
+      </section>
+    </>
+  );
 }
 
 export default App;
