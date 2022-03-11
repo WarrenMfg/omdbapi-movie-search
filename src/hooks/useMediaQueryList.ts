adc9a4f203a42f0b959a027bec1cc5e577a42ca5
WarrenMfg <warrenmfg@icloud.com>
2022-03-11 14:20:26 -0500

    removes useMediaQueryList

--- src/hooks/useMediaQueryList.ts
+++
@@ -1,6 +0,0 @@
-const useMediaQueryList = (mediaQuery: string, handleOnChange: () => void) => {
-  const mql = window.matchMedia(mediaQuery);
-  mql.onchange = handleOnChange;
-};
-
-export default useMediaQueryList;
