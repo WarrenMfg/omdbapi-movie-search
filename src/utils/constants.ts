0c537dc330d24f5f472a8bff97d406f7d5089a26
WarrenMfg <warrenmfg@icloud.com>
2022-03-13 06:24:56 -0400

    updates button styles

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const BUTTON_STYLE =
-  'inline-block rounded-lg bg-sky-900 py-4 px-8 text-cyan-100 transition-colors hover:bg-cyan-700';
+  'inline-block rounded-lg bg-sky-900 py-4 px-8 text-cyan-100 transition-colors tl:hover:bg-cyan-700';
 export const FAVORITES = 'favorites';
 export const MOVIE_DETAILS_PROPERTIES = [
   'Rated',
