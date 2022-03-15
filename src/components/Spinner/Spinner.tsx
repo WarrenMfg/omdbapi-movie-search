067a3781a9e2ed2186e58f71b7f19dc374360f0c
WarrenMfg <warrenmfg@icloud.com>
2022-03-14 20:47:59 -0400

    removes data-cy attribute

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -3,7 +3,7 @@
  */
 const Spinner = () => {
   return (
-    <div className='grid h-full place-items-center' data-cy='spinner'>
+    <div className='grid h-full place-items-center'>
       <span className='inline-block h-12 w-12 animate-spin rounded-full border-8 border-cyan-700 border-b-cyan-100' />
     </div>
   );
