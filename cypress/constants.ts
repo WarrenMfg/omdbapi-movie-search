f5fab8eb75304197ed9a69b622a5f3e601b7fef5
WarrenMfg <warrenmfg@icloud.com>
2022-03-14 20:48:15 -0400

    updates spinner selector

--- cypress/constants.ts
+++ cypress/constants.ts
@@ -11,6 +11,6 @@ export const MBP16 = {
 export const MOBILE_NAV_BUTTON = '[data-cy="mobile-nav-button"]';
 export const MOVIE_LIST = '[data-cy="movie-list"]';
 export const MODAL_BACKGROUND = '[data-cy="modal-background"]';
-export const SPINNER = '[data-cy="spinner"]';
+export const SPINNER = '[data-cy="modal-spinner"]';
 export const MOBILE_NAV = '[data-cy="mobile-nav"]';
 export const DESKTOP_NAV = '[data-cy="desktop-nav"]';
