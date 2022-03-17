da608285ff1aa6eeeada769fb50cda10257b87f2
WarrenMfg <warrenmfg@icloud.com>
2022-03-16 20:56:43 -0400

    adds basic tests

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import Button from './Button';
+
+describe('Button', () => {
+  const hello = /hello/i;
+
+  it('should render', () => {
+    render(<Button>Hello</Button>);
+    expect(screen.getByText(hello)).toBeVisible();
+  });
+
+  it('should accept an aria-label, id, and classes', () => {
+    const ariaLabel = 'aria label';
+    const id = 'id';
+    const className = 'className';
+    render(
+      <Button ariaLabel={ariaLabel} id={id} className={className}>
+        Hello
+      </Button>
+    );
+    const el = screen.getByText(hello);
+    expect(el).toHaveAttribute('aria-label', ariaLabel);
+    expect(el).toHaveAttribute('id', id);
+    expect(el).toHaveAttribute('class', className);
+  });
+
+  it('should handle clicks', () => {
+    const handleOnClick = jest.fn();
+    render(<Button handleOnClick={handleOnClick}>Hello</Button>);
+    screen.getByText(hello).click();
+    expect(handleOnClick).toHaveBeenCalled();
+  });
+});
---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react';
+import Card from './Card';
+
+const cardProps = {
+  handleOpenCard: jest.fn(),
+  title: 'title',
+  isFavorite: false,
+  year: '1970',
+  image: 'https://plchldr.co/i/300x300?bg=cffafe&fc=0e7490&text=Oops',
+  id: 'id',
+  ariaSetSize: 1,
+  ariaPosInSet: 1,
+};
+
+describe('Card', () => {
+  it('should render', () => {
+    render(<Card {...cardProps} />);
+    expect(screen.getByTestId('card')).toBeInTheDocument();
+  });
+
+  it('should contain a button and handle a click', () => {
+    render(<Card {...cardProps} />);
+    screen.getByText(cardProps.title).click();
+    expect(cardProps.handleOpenCard).toHaveBeenCalled();
+  });
+
+  it('should display the title and year', () => {
+    render(<Card {...cardProps} />);
+    expect(screen.getByText(cardProps.title)).toBeVisible();
+    expect(screen.getByText(cardProps.year)).toBeVisible();
+  });
+
+  it('should display an image', () => {
+    render(<Card {...cardProps} />);
+    const altRegEx = /.*title.*/i;
+    expect(screen.getByAltText(altRegEx)).toHaveAttribute(
+      'alt',
+      expect.stringMatching(altRegEx)
+    );
+  });
+});
