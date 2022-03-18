8050fbc05fd56d7e81a9f05daf7ae8831faa92d7
WarrenMfg <warrenmfg@icloud.com>
2022-03-18 12:23:24 -0400

    adds testids

--- src/components/HamburgerIcon/HamburgerIcon.tsx
+++ src/components/HamburgerIcon/HamburgerIcon.tsx
@@ -11,18 +11,21 @@ const hamburgerBaseClasses = 'bg-cyan-100 h-0.5 transition-transform';
  */
 const HamburgerIcon = ({ isMobileNavOpen }: HamburgerIconProps) => {
   return (
-    <div className='w-8 p-2' data-testid='mobile-nav-button'>
+    <div className='w-8 p-2' data-testid='hamburger-icon'>
       <div
+        data-testid='hamburger-icon-top'
         className={cn(hamburgerBaseClasses, {
           'translate-y-1.5 rotate-45': isMobileNavOpen,
         })}
       ></div>
       <div
+        data-testid='hamburger-icon-middle'
         className={cn(hamburgerBaseClasses, 'my-1 transition-opacity', {
           'opacity-0': isMobileNavOpen,
         })}
       ></div>
       <div
+        data-testid='hamburger-icon-bottom'
         className={cn(hamburgerBaseClasses, {
           '-translate-y-1.5 -rotate-45': isMobileNavOpen,
         })}
