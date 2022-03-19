4ae0b3534a80f3ec80bc28a61fa8c87095c4b894
WarrenMfg <warrenmfg@icloud.com>
2022-03-19 06:37:40 -0400

    elevates logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,43 +1,31 @@
-import useDispatch from '../../hooks/useDispatch';
-import useSelector from '../../hooks/useSelector';
-import {
-  CLOSE_MOBILE_NAV,
-  OPEN_MOBILE_NAV,
-  setMobileNav,
-} from '../../state/mobileNav/mobileNavActions';
 import Button from '../Button/Button';
 import HamburgerIcon from '../HamburgerIcon/HamburgerIcon';
+
+interface HeaderProps {
+  handleOnClick: () => void;
+  isMobileNavOpen: boolean;
+}
 
 /**
  * Top bar sticky header
  */
-const Header = () => {
-  const dispatch = useDispatch();
-  const isMobileNavOpen = useSelector<boolean>(state => state.mobileNav.isOpen);
-
-  // Mobile nav button click handler
-  const handleOnClick = () =>
-    dispatch(
-      setMobileNav(isMobileNavOpen ? CLOSE_MOBILE_NAV : OPEN_MOBILE_NAV)
-    );
-
-  return (
-    <header className='sticky top-0 z-20 flex items-center text-center tl:text-left'>
-      <div className='absolute left-4 flex items-center tl:hidden'>
-        <Button
-          handleOnClick={handleOnClick}
-          ariaLabel={`${isMobileNavOpen ? 'Close' : 'Open'} mobile navigation`}
-          ariaControls='main-menu'
-          ariaExpanded={isMobileNavOpen}
-        >
-          <HamburgerIcon isMobileNavOpen={isMobileNavOpen} />
-        </Button>
-      </div>
-      <h1 className='grow bg-cyan-700 py-2 pl-4 text-xl uppercase text-cyan-100'>
-        Movie Search
-      </h1>
-    </header>
-  );
-};
+const Header = ({ handleOnClick, isMobileNavOpen }: HeaderProps) => (
+  <header className='sticky top-0 z-20 flex items-center text-center tl:text-left'>
+    <div className='absolute left-4 flex items-center tl:hidden'>
+      <Button
+        handleOnClick={handleOnClick}
+        ariaLabel={`${isMobileNavOpen ? 'Close' : 'Open'} mobile navigation`}
+        ariaControls='mobile-nav'
+        ariaExpanded={isMobileNavOpen}
+        id='mobile-nav-button'
+      >
+        <HamburgerIcon isMobileNavOpen={isMobileNavOpen} />
+      </Button>
+    </div>
+    <h1 className='grow bg-cyan-700 py-2 pl-4 text-xl uppercase text-cyan-100'>
+      Movie Search
+    </h1>
+  </header>
+);
 
 export default Header;
